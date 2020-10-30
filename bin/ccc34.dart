19f2754867f7f3bc7c4c254ae8efece3456e3b65
TheKingDave |_ <david@langheiter.com>
2020-10-30 21:55:17 +0100

    Level2

--- bin/ccc34.dart
+++ bin/ccc34.dart
@@ -7,19 +7,21 @@ void main(List<String> arguments) async {
   final dry = arguments.isNotEmpty;
 
   final path = 'files/level2/level2_';
-  
-  await runFile('${path}example', dry);
-  
-  for(var i = 1; i <= 5; i++) {
-    //await runFile('$path$i', dry);
+
+  if (dry) {
+    await runFile('${path}example', dry);
+    return;
   }
-  
+
+  for (var i = 1; i <= 5; i++) {
+    await runFile('$path$i', dry);
+  }
 }
 
 void runFile(String path, bool dry) async {
   final solution = await runSolution('$path.in');
 
-  if(dry) {
+  if (dry) {
     print('Solution:');
     print(solution);
     return;
@@ -31,27 +33,44 @@ void runFile(String path, bool dry) async {
 Future<String> runSolution(String path) async {
   final file = File(path);
 
-  if(!await file.exists()) {
+  if (!await file.exists()) {
     print('File "${file.absolute}" does not exist');
     throw ArgumentError('File "${file.absolute}" does not exist');
   }
   print('File exists');
 
-  final input = StreamQueue<String>(file.openRead().transform(utf8.decoder).transform(LineSplitter()));
-  
+  final input = StreamQueue<String>(
+      file.openRead().transform(utf8.decoder).transform(LineSplitter()));
+
   final costNum = int.parse(await input.next);
-  var costs = await input.take(costNum);
+  var costs = (await input.take(costNum)).map(int.parse).toList();
   final taskNum = int.parse(await input.next);
   var tasks = (await input.take(taskNum)).map((e) {
     final split = e.split(' ').map(int.parse).toList();
     return Tuple2(split[0], split[1]);
   });
-  
-  final taskMin = <int, int>{};
-  
-  for(var i = 0; i < costNum; i++) {
-    
-  }
-  
-  return '0';
+
+  final costMap = <int, int>{};
+
+  tasks.forEach((task) {
+    var minCost = 999999;
+    var minMinute = 0;
+    for (var i = 0; i < costNum - task.item2+1; i++) {
+      final subCost = costs
+          .getRange(i, i + task.item2)
+          .fold(0, (previousValue, cost) => previousValue + cost);
+
+      if(subCost < minCost) {
+        minCost = subCost;
+        minMinute = i;
+      }
+    }
+    costMap[task.item1] = minMinute;
+  });
+
+  var ret = '${tasks.length}\n';
+
+  ret += costMap.entries.map((e) => '${e.key} ${e.value}').join('\n');
+
+  return ret;
 }
